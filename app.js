2e9a366caf78fdc6df5853df7acac48fcec12c87
Spycrab-db <gavinyan2004@gmail.com>
2023-06-10 21:18:22 -0400

    Setup connection to MongoDB

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const app = express();
+const CampGround = require("./models/camp-ground");
+
+try{
+    mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp').then(()=>{
+        console.log("DATABASE CONNECTED");
+    })
+} catch(e){
+    console.log("ERROR CONNECTING TO MONGODB");
+    console.log(e);
+}
 
 app.set('view engine', "ejs");
 app.set('view', path.join(__dirname, 'views'));
