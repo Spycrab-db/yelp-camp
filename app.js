76458578f3b7ae1bc8987e64cd17ee7af99ae0e0
Spycrab-db <gavinyan2004@gmail.com>
2023-06-10 23:19:40 -0400

    Added logic for editing campgrounds

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const app = express();
 const Campground = require("./models/camp-ground");
 
@@ -9,7 +10,7 @@ const Campground = require("./models/camp-ground");
 try{
     mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp').then(()=>{
         console.log("DATABASE CONNECTED");
-    })
+    });
 } catch(e){
     console.log("ERROR CONNECTING TO MONGODB");
     console.log(e);
@@ -19,6 +20,7 @@ try{
 app.set('view engine', "ejs");
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 
 //Home route
 app.get('/', (req, res)=>{
@@ -32,19 +34,31 @@ app.get('/campgrounds', async (req, res)=>{
 //Render new campground route
 app.get('/campgrounds/new', async (req, res)=>{
     res.render('campgrounds/new');
-})
+});
 //Post new campground route
 app.post('/campgrounds', async (req, res)=>{
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
-})
+});
 //Show campground route
 app.get('/campgrounds/:id', async (req, res)=>{;
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/show', {campground});
 });
 
+//Render edit campground route
+app.get('/campgrounds/:id/edit', async (req, res)=>{;
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/edit', {campground});
+});
+//Edit campground route
+app.put('/campgrounds/:id', async (req, res)=>{
+    const { id } = req.params;
+    await Campground.findByIdAndUpdate(id, req.body.campground);
+    res.redirect(`/campgrounds/${id}`);
+});
+//Confirm server is running
 app.listen(3000, ()=>{
     console.log("LISTENING AT PORT 3000");
 });
