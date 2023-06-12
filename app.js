d649d7c26d26dd94c49316d45f6e34ee98ee700f
Spycrab-db <gavinyan2004@gmail.com>
2023-06-11 20:59:54 -0400

    Added generic error handling to prevent server crashing

--- app.js
+++ app.js
@@ -3,16 +3,18 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const ExpressError = require('./utils/ExpressError');
+const wrapAsync = require('./utils/WrapAsync');
 const methodOverride = require('method-override');
 const app = express();
 const Campground = require("./models/camp-ground");
 
 //Connect to MongoDB
-try{
-    mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp').then(()=>{
+try {
+    mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp').then(() => {
         console.log("DATABASE CONNECTED");
     });
-} catch(e){
+} catch (e) {
     console.log("ERROR CONNECTING TO MONGODB");
     console.log(e);
 }
@@ -21,52 +23,58 @@ try{
 app.set('view engine', "ejs");
 app.engine('ejs', ejsMate);
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
 //Home route
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send("HOME");
 });
 //Campgrounds route
-app.get('/campgrounds', async (req, res)=>{
+app.get('/campgrounds', wrapAsync(async (req, res) => {
     const campgrounds = await Campground.find({});
-    res.render('campgrounds/index', {campgrounds});
-});
+    res.render('campgrounds/index', { campgrounds });
+}));
 //Render new campground route
-app.get('/campgrounds/new', async (req, res)=>{
+app.get('/campgrounds/new', wrapAsync(async (req, res) => {
     res.render('campgrounds/new');
-});
+}));
 //Post new campground route
-app.post('/campgrounds', async (req, res)=>{
+app.post('/campgrounds', wrapAsync(async (req, res) => {
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
-});
+}));
 //Show campground route
-app.get('/campgrounds/:id', async (req, res)=>{;
+app.get('/campgrounds/:id', wrapAsync(async (req, res) => {
+    ;
     const campground = await Campground.findById(req.params.id);
-    res.render('campgrounds/show', {campground});
-});
-
+    res.render('campgrounds/show', { campground });
+}));
 //Render edit campground route
-app.get('/campgrounds/:id/edit', async (req, res)=>{;
+app.get('/campgrounds/:id/edit', wrapAsync(async (req, res) => {
+    ;
     const campground = await Campground.findById(req.params.id);
-    res.render('campgrounds/edit', {campground});
-});
+    res.render('campgrounds/edit', { campground });
+}));
 //Edit campground route
-app.put('/campgrounds/:id', async (req, res)=>{
+app.put('/campgrounds/:id', wrapAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndUpdate(id, req.body.campground);
     res.redirect(`/campgrounds/${id}`);
-});
+}));
 //Delete campground route
-app.delete('/campgrounds/:id', async (req, res)=>{
+app.delete('/campgrounds/:id', wrapAsync(async (req, res) => {
     const { id } = req.params;
     deletedCamp = await Campground.findByIdAndDelete(id);
     res.redirect(`/campgrounds`);
+}));
+
+//Error handler
+app.use((err, req, res, next) => {
+    res.send("SOMETHING WENT WRONG!");
 })
 //Confirm server is running
-app.listen(3000, ()=>{
+app.listen(3000, () => {
     console.log("LISTENING AT PORT 3000");
 });
